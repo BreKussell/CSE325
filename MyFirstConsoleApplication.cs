8f73724e411ecfc44b8783dce983a88a19fab5b0
Breanna Russell <brekayussell@gmail.com>
2024-09-21 16:46:59 -0600

    commit

--- MyFirstConsoleApplication.cs
+++ MyFirstConsoleApplication.cs
@@ -24,19 +24,6 @@ namespace MyFirstConsoleApplication
 
             // Display days until Christmas
             Console.WriteLine($"There are {daysUntilChristmas} days until Christmas this year.");
-            
-            // Program example from section 2.1 in the C# Programming Yellow Book (Rob Miles)
-            // Asking for input and calculating the area of a rectangle
-            Console.Write("Enter the width of the rectangle: ");
-            double width = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Enter the height of the rectangle: ");
-            double height = Convert.ToDouble(Console.ReadLine());
-
-            double area = width * height;
-
-            // Output the calculated area with a label
-            Console.WriteLine($"The area of the rectangle is {area} square units.");
 
             // Prevent the console from closing immediately
             Console.WriteLine("Press any key to exit...");
